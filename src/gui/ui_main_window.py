ee20359c7e189820cd66f6b58aedd7c5dc6be6c7
Abraamyan Alexander <samvelcc25@outlook.com>
2024-05-28 16:54:28 +0300

    resize

--- src/gui/ui_main_window.py
+++ src/gui/ui_main_window.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 950)
+        MainWindow.resize(800, 1000)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.connect_btn = QtWidgets.QPushButton(self.centralwidget)
